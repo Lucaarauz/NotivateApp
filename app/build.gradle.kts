8151e9fd7ad6857a77d117eb8bbb67b9f199871e
Lucaarauz <166809332+Lucaarauz@users.noreply.github.com>
2024-07-10 11:41:53 +0200

    Version 1.03 - New Minimum SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "notivate.com"
-        minSdk = 29
+        minSdk = 33
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
