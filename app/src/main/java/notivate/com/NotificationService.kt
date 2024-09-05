cf847ba23c1728041d7b02c4557c56eab70c938b
Lucaarauz <35530@lln.hethwc.nl>
2024-09-05 14:32:57 +0200

    Attempt at adding a delay with alarm manager

--- app/src/main/java/notivate/com/NotificationService.kt
+++ app/src/main/java/notivate/com/NotificationService.kt
@@ -66,16 +66,14 @@ class NotificationService : Service() {
     }
 
     private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = "Notification Channel"
-            val descriptionText = "Channel for sending notifications"
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
-                description = descriptionText
-            }
-
-            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
+        val name = "Notification Channel"
+        val descriptionText = "Channel for sending notifications"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+            description = descriptionText
         }
+
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
     }
 }
